f3c8cd1e8fa79aad2b0c9b88582fe7a5bd29a688
HRG <hreyes.gopar@gmail.com>
2019-11-14 18:27:49 -0600

    add variables used by mkfile

---
+++ config.mk
@@ -0,0 +1,11 @@
+# Configuration variables to run hicup truncator
+#
+# number of CPUs to use
+HM_THREADS_NUMBER = 10
+
+# bowtie2 executable path
+BOWTIE2_PATH = 
+
+# bowtie2 index path
+BOWTIE2_INDEX_PATH = 
+
