b3ed7e13e0f392eafea9742f0ac045a4a874e581
HRG <hreyes.gopar@gmail.com>
2019-11-21 16:50:36 -0600

    add example variables

--- config.mk
+++ config.mk
@@ -1,11 +1,11 @@
 # Configuration variables to run hicup truncator
 #
 # number of CPUs to use
-HM_THREADS_NUMBER = 10
+HM_THREADS_NUMBER = 8
 
 # bowtie2 executable path
-BOWTIE2_PATH = 
+BOWTIE2_PATH = /usr/bin/bowtie2
 
-# bowtie2 index path
-BOWTIE2_INDEX_PATH = 
+#[path to folder]/[name of folder containing indices]/[index basename]
+BOWTIE2_INDEX_PATH = /home/hreyes/reference-files/Homo_sapiens/NCBI/GRCh38/Sequence/Bowtie2Index/genome
 
